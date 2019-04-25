443189af8fe24a09103b8d4dc1a6dc6e940ba253
VicenzoMartinelli <vicenzomartinelli@hotmail.com>
2019-04-24 22:08:10 -0300

    #1

---
+++ src/main/java/br/edu/utfpr/pb/trabalhofinalweb1/TrabalhofinalWeb1Application.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.trabalhofinalweb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhofinalWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhofinalWeb1Application.class, args);
+	}
+
+}
